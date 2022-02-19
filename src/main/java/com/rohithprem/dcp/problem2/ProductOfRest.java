e713bb3137f1ee93c2aa73a2be91b3eda874e524
rohithprem <rohithp@live.com>
2022-02-19 18:32:22 +0000

    Problem#2 with basic answer and test cases

---
+++ src/main/java/com/rohithprem/dcp/problem2/ProductOfRest.java
@@ -0,0 +1,46 @@
+package com.rohithprem.dcp.problem2;
+
+/**
+ * Daily Coding Problem: Problem #2 [Hard]
+ *
+ * This problem was asked by Uber.
+ *
+ * Given an array of integers, return a new array such that each element at index i of the new array is the product of
+ * all the numbers in the original array except the one at i.
+ *
+ * For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24].
+ * If our input was [3, 2, 1], the expected output would be [2, 3, 6].
+ *
+ * Follow-up: what if you can't use division?
+ */
+
+public class ProductOfRest {
+
+    //With division
+    public Integer[] executeWithDivision(Integer[] input){
+        Integer[] output = new Integer[input.length];
+        int totalProduct = 1;
+        int countOfZeroes = 0;
+        for(int value : input){
+            if(value != 0){
+                totalProduct *= value;
+            } else {
+                countOfZeroes++;
+                if(countOfZeroes > 1){
+                    java.util.Arrays.fill(output, 0);
+                    return output;
+                }
+            }
+        }
+        for(int i=0 ; i<input.length ; i++){
+            int number = input[i];
+            if(number != 0){
+                output[i] = countOfZeroes == 0 ? totalProduct/input[i] : 0;
+            } else {
+                output[i] = totalProduct;
+            }
+        }
+        return output;
+    }
+
+}
---
+++ src/test/java/com/rohithprem/dcp/problem2/ProductOfRestTest.java
@@ -0,0 +1,56 @@
+package com.rohithprem.dcp.problem2;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Daily Coding Problem: Problem #2 [Hard]
+ *
+ * This problem was asked by Uber.
+ *
+ * Given an array of integers, return a new array such that each element at index i of the new array is the product of
+ * all the numbers in the original array except the one at i.
+ *
+ * For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24].
+ * If our input was [3, 2, 1], the expected output would be [2, 3, 6].
+ *
+ * Follow-up: what if you can't use division?
+ */
+
+public class ProductOfRestTest {
+
+    private static final ProductOfRest productOfRest = new ProductOfRest();
+
+    @Test
+    public void testProductOfRestSimpleGivenExample1(){
+        Integer[] input = {1,2,3,4,5};
+        Integer[] output = productOfRest.executeWithDivision(input);
+        Integer[] expectedOutput = {120,60,40,30,24};
+        Assert.assertArrayEquals(output, expectedOutput);
+    }
+
+    @Test
+    public void testProductOfRestSimpleGivenExample2(){
+        Integer[] input = {3,2,1};
+        Integer[] output = productOfRest.executeWithDivision(input);
+        Integer[] expectedOutput = {2,3,6};
+        Assert.assertArrayEquals(output, expectedOutput);
+    }
+
+    @Test
+    public void testProductOfRestWithSingleZero(){
+        Integer[] input = {0,4,10};
+        Integer[] output = productOfRest.executeWithDivision(input);
+        Integer[] expectedOutput = {40,0,0};
+        Assert.assertArrayEquals(output, expectedOutput);
+    }
+
+    @Test
+    public void testProductOfRestWithMultipleZeroes(){
+        Integer[] input = {20,1,30,0,400,2,0,15};
+        Integer[] output = productOfRest.executeWithDivision(input);
+        Integer[] expectedOutput = {0,0,0,0,0,0,0,0};
+        Assert.assertArrayEquals(output, expectedOutput);
+    }
+
+}
